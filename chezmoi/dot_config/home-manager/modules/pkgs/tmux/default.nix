ad123fed1fdc3e906226633e73b6031683171663
ohma <ohmaevepk@gmail.com>
2026-06-17 17:11:40 +0800

    fix typo

--- chezmoi/dot_config/home-manager/modules/pkgs/tmux/default.nix
+++ chezmoi/dot_config/home-manager/modules/pkgs/tmux/default.nix
@@ -1,27 +1,28 @@
 { config, pkgs, ... }:
-{
-  _class = "homeManager";
+  {
+    _class = "homeManager";
 
-  programs.tmux.enable = true;
+    programs.tmux.enable = true;
 
-  programs.tmux.tmuxinator.enable = true;
-  programs.tmux = {
+    programs.tmux = {
 
-  # 插件列表
-  plugins = with pkgs.tmuxPlugins; [
-    # 1. 简单写法：直接写插件包名
-    sensible
-    # 2. 高级写法：插件自带额外的 tmux 配置
-    {
-      plugin = resurrect;
-      extraConfig = "set -g @resurrect-strategy-vim 'session'";
-    }
-    {
-      plugin = continuum;
-      extraConfig = ''
-        set -g @continuum-restore 'on'
-        set -g @continuum-save-interval '60' # 60分钟保存一次
-      '';
-    }
-  ];
-};
+    tmuxinator.enable = true;
+    # 插件列表
+    plugins = with pkgs.tmuxPlugins; [
+      # 1. 简单写法：直接写插件包名
+      sensible
+      # 2. 高级写法：插件自带额外的 tmux 配置
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-vim 'session'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60' # 60分钟保存一次
+        '';
+      }
+    ];
+  };
+}
